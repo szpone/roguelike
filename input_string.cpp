2cfae6a5f5eabacfc4c4c2c275203658a746e9d9
szpone <nikola.adamus@gmail.com>
2017-10-14 15:49:10 +0200

    take input and print string

---
+++ input_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::string s;
+    std::cout << "Enter a string: ";
+    std::cin >> s;
+    std::cout << "You entered: " << s << "\n";
+
+    return 0;
+}
