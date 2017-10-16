12fdacfdd6ac059caf732921cbd8a631c04a8a3b
szpone <nikola.adamus@gmail.com>
2017-10-16 12:58:06 +0200

    edit travis config, add clang-format

--- loop.cpp
+++ loop.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
-    std::cout << "Enter your first name: ";
-    std::string name;
-    std::cin >> name;
+int main() {
+  std::cout << "Enter your first name: ";
+  std::string name;
+  if (std::cin >> name) { ok(); } else { error(); }
 
-    const std::string greeting = "Hello " + name;
+  const std::string greeting = "Hello " + name;
 
-    for (int i = 0; greeting[i] != '\0'; i++) {
-        std::cout << greeting[i] << std::endl;
-    }
+  for (int i = 0; greeting[i] != '\0'; i++) {
+    std::cout << greeting[i] << std::endl;
+  }
 
-    return 0;
+  return 0;
 }
