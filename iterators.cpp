5957f03eff823eb7c97422ab4d5e61b21cf81f2f
szpone <nikola.adamus@gmail.com>
2017-10-26 22:11:16 +0200

    iterators

---
+++ iterators.cpp
@@ -0,0 +1,18 @@
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main() {
+    std::int number_one 1;
+    std::int number_2 2;
+
+    const std::vector numbers = numbers.push_back(number_one)
+
+    std::cout << numbers << std::endl;
+
+
+  return 0;
+}
