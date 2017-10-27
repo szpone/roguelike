c3ce2927e431cae5f16ee36f4b8f83fa058ad841
szpone <nikola.adamus@gmail.com>
2017-10-27 19:32:11 +0200

    vectors and iterators

--- iterators.cpp
+++ iterators.cpp
@@ -1,35 +1,16 @@
-// #include <iomanip>
-// #include <ios>
-// #include <iostream>
-// #include <string>
-// #include <vector>
-
-
-// int main() {
-//     std::int number_one 1;
-//     std::int number_2 2;
-
-//     const std::vector numbers = numbers.push_back(number_one)
-
-//     std::cout << numbers << std::endl;
-
-
-//   return 0;
-// }
-
 #include <iostream>
 #include <vector>
 
 int main() {
+    int x;
 
-    std::string dupa = "ioioio";
-    // std::vector wektor = wektor.push_back(dupa);
-    std::vector<string> word;
+    std::vector<int> v;
 
-    word.push_back(dupa);
+    std::cout << 'Enter as many number as you want: '
+
+    while (std::cin >> x) {
+        v.push_back(x);
+    }
 
 
-    std::cout << word << std::endl;
-
-    return 0;
 }
