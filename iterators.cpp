b167cdc86f4ddc73190c3a3f12148b4f22e71c26
szpone <nikola.adamus@gmail.com>
2017-10-28 18:27:03 +0200

    iterators exercises

--- iterators.cpp
+++ iterators.cpp
@@ -2,42 +2,106 @@
 #include <vector>
 #include <iterator>
 
-int main() {
-    int x;
 
-    std::vector<int> v;
+//reverse iteration
 
-    std::cout << "Enter as many number as you want:\n";
+// int main() {
 
-    while (std::cin >> x) {
-        v.push_back(x);
-    }
+//     std::vector<int> v = {1, 2, 3, 4};
+//     auto it = v.rbegin();
+//     auto start = v.rend();
 
-    // std::size_t i = 0;
-    // std::size_t greatest_index = 0;
-    // std::size_t least_index = 0;
-    // while (i < v.size()) {
-    //     if (v[i] > v[greatest_index])
-    //         greatest_index = i;
-    //     if (v[i] < v[least_index])
-    //         least_index = i;
-    //     i += 1;
-    // }
+//     for(; it != start; it++) {
+//         std::cout << *it << std::endl;
+//     }
 
-    for (vector<v>::const_iterator iter v.begin();
-        iter != students.end(); ++iter) {
-        cout << (*iter) << endl;
-    }
+//     return 0;
+// }
 
 
+// iterators for string
 
-    // if (v.empty()) {
-    //     std::cout << "An empty sequence has no greatest or least element.\n";
-    // } else {
-    //     std::cout << "Greatest element " << v[greatest_index] << " is at index " << greatest_index << ".\n";
-    //     std::cout << "Least element " << v[least_index] << " is at index " << least_index << ".\n";
-    // }
+// int main() {
+//     std::string s = "Ala ma kota";
 
-    return 0;
+//     for(auto it = s.begin(); it != s.end(); it++) {
+//         std::cout << *it << std::endl;
+//     }
 
-}
+//     return 0;
+// }
+
+
+// How do iterators describe ranges of items?
+
+// By the start() and end() methods which points out the end and beginning of for example, a vector
+
+// How do iterators look like for empty ranges?
+
+// int main() {
+//     std::vector<int> v = {};
+//     auto start = v.begin();
+//     auto end = v.end();
+
+//     std::cout << *start << std::endl;
+
+//     return 0;
+// }
+
+
+// How to iterate over
+
+// int main() {
+
+//     std::vector<int> v = {1, 2, 3, 4, 5};
+//     auto start = v.begin();
+//     auto end = v.end();
+
+//     for (; start != end; start++) {
+//         std::cout << *start << std::endl;
+//     }
+
+
+//     return 0;
+// }
+
+
+
+
+
+// int main() {
+//     int x;
+
+//     std::vector<int> v;
+
+//     std::cout << "Enter as many number as you want:\n";
+
+//     while (std::cin >> x) {
+//         v.push_back(x);
+//     }
+
+//     std::size_t i = 0;
+//     std::size_t greatest_index = 0;
+//     std::size_t least_index = 0;
+//     while (i < v.size()) {
+//         if (v[i] > v[greatest_index])
+//             greatest_index = i;
+//         if (v[i] < v[least_index])
+//             least_index = i;
+//         i += 1;
+//     }
+
+
+
+
+//     if (v.empty()) {
+//         std::cout << "An empty sequence has no greatest or least element.\n";
+//     } else {
+//         std::cout << "Greatest element " << v[greatest_index] << " is at index " << greatest_index << ".\n";
+//         std::cout << "Least element " << v[least_index] << " is at index " << least_index << ".\n";
+//     }
+
+//     return 0;
+
+// }
+// for (auto start = std::begin(v), end = std::end(v); start != end; start++)
