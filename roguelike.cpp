bf43d86faee6eb4e668cc8d7d6039d83404ea990
szpone <nikola.adamus@gmail.com>
2018-01-17 10:46:21 +0100

    add 5 monsters and move thme

--- roguelike.cpp
+++ roguelike.cpp
@@ -1,7 +1,13 @@
 #include "PerlinNoise.hpp"
+#include <cstdlib>
 #include <iostream>
 #include <ncurses.h>
 #include <vector>
+
+class Map;
+void draw_map(Map &map);
+void find_initial_position(Map &map, int &x, int &y);
+void pick_monster_position(Map &map, int &x, int &y);
 
 class Map {
 public:
@@ -17,13 +23,12 @@ public:
     this->width = width;
     this->height = height;
 
-    siv::PerlinNoise perlin(10);
+    siv::PerlinNoise perlin(42);
 
-    // data.resize(width * height);
     for(int y = 0; y < height; ++y) {
       for(int x = 0; x < width; ++x) {
         double val = perlin.octaveNoise0_1((0.5+x / (double)width) * 5, (0.5 + y / (double)height) * 5, 2);
-        if(val < 0.5) {
+        if(val < 0.333) {
           data.push_back('#');
         } else {
           data.push_back('.');
@@ -33,10 +38,144 @@ public:
   }
 };
 
-void game_loop(Map &map, char main_char, int y, int x, int ch);
-void draw_map(Map &map);
-void find_initial_position(Map &map, int &x, int &y);
-void erasing(int y, int x);
+class Monster {
+public:
+  char monster_char = '&';
+  int x, y;
+  int hp;
+  int attack; 
+
+  Monster(int x, int y): x(x), y(y) {
+  }
+};
+
+
+enum {
+  UP = 0,
+  DOWN = 1,
+  LEFT = 2,
+  RIGHT = 3,
+};
+
+
+class Game {
+public:
+  char main_char = '@';
+  std::vector<Monster> monsters;
+  Map &map;
+  int x, y;
+
+  Game(Map &map, int x, int y): map(map), x(x), y(y) {}
+
+  void generate_monsters() {
+    for(int i = 0; i < 5; ++i) {
+      int x, y;
+      pick_monster_position(map, x, y);
+      monsters.push_back(Monster(x, y));
+    }
+  }
+
+  void draw_monsters() {
+    for(Monster &monster: monsters) {
+      mvaddch(monster.y, monster.x, monster.monster_char);
+    }
+  }
+
+  void move_monsters() {
+    for(Monster &monster: monsters) {
+      int direction = rand() % 4;
+      if(direction == UP) {
+        move_monster(monster, monster.x, monster.y - 1);
+      } else if(direction == DOWN) {
+        move_monster(monster, monster.x, monster.y + 1);
+      } else if(direction == LEFT) {
+        move_monster(monster, monster.x - 1, monster.y);
+      } else if(direction == RIGHT) {
+        move_monster(monster, monster.x + 1, monster.y);
+        }
+    }
+  }
+
+  bool check_position(int new_x, int new_y) {
+    if (map.getChar(new_x, new_y) == '.' && !has_monster(new_x, new_y) && 
+        new_x < map.width && new_y < map.height && new_x >= 0 && new_y >= 0) {
+      return true;
+    }
+    return false;
+  }
+
+  void move_hero(int new_x, int new_y) {
+    if (check_position(new_x, new_y)) {
+      x = new_x;
+      y = new_y;
+    }
+  }
+
+  bool has_monster(int x, int y) {
+    for (Monster &monster : monsters) {
+      if(monster.x == x && monster.y == y) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void move_monster(Monster &monster, int new_x, int new_y) {
+    if(check_position(new_x, new_y)) {
+      monster.x = new_x;
+      monster.y = new_y;
+    }
+
+  }
+
+  void game_loop(int ch) {
+
+    if (ch == 'q' || ch == 'Q')
+      return;
+
+    mvaddch(y, x, main_char);
+    refresh();
+
+    for (;;) {
+      redraw();
+
+      ch = getch();
+
+      if (ch == KEY_LEFT) {
+        move_hero(x - 1, y);
+
+      } else if (ch == KEY_RIGHT) {
+        move_hero(x + 1, y);
+
+      } else if (ch == KEY_UP) {
+        move_hero(x, y - 1);
+
+
+      } else if (ch == KEY_DOWN) {
+        move_hero(x, y + 1);
+      } else if (ch == 'q' || 'Q') {
+        break;
+      }
+      move_monsters();
+      draw_monsters();
+    }
+  }
+  void erasing(int y, int x) { mvaddch(y, x, ' '); }
+
+  void redraw() {
+    clear();
+    draw_map(map);
+    draw_monsters();
+    draw_player();
+    refresh();
+  }
+
+  void draw_player() {
+    mvaddch(y, x, main_char);
+  }
+};
+
+
 
 
 int main() {
@@ -46,7 +185,6 @@ int main() {
   raw();
   keypad(stdscr, TRUE);
   curs_set(0);
-  char main_char = '@';
   printw("Welecome to awesome roguelike");
   initscr();
   
@@ -55,22 +193,15 @@ int main() {
   int width, height;
   getmaxyx(stdscr, height, width);
   
-
   Map map(width, height);
-  draw_map(map);
   int y, x;
   find_initial_position(map, x, y);
-  game_loop(map, main_char, y, x, ch);
+  Game game(map, x, y);
+  game.generate_monsters();
+  game.game_loop(ch);
   endwin();
   return 0;
 
-
-  /*
-  Map map(width, height);
-  Game game(map, '@');
-  Game.loop();
-
-  */
 }
 
 void find_initial_position(Map &map, int &xPos, int &yPos) {
@@ -85,61 +216,16 @@ void find_initial_position(Map &map, int &xPos, int &yPos) {
   }
 }
 
+void pick_monster_position(Map &map, int &xPos, int &yPos) {
+  xPos = rand() % map.width;
+  yPos = rand() % map.height;
+}
+
 void draw_map(Map &map) {
   for(int x = 0; x < map.width; x++) {
     for(int y = 0; y < map.height; y++) {
-      //mvaddch(j, i, map.data[j * map.width + i]);
       mvaddch(y, x, map.getChar(x, y));
     }
   }
 }
 
-
-
-
-void game_loop(Map &map, char main_char, int y, int x, int ch) {
-
-  if (ch == 'q' || ch == 'Q')
-    return;
-
-  mvaddch(y, x, main_char);
-  refresh();
-
-  for (;;) {
-    ch = getch();
-
-    if (ch == KEY_LEFT) {
-      erasing(y, x);
-      if(map.getChar(x - 1, y) == '.') {
-        x = x - 1;
-      }
-      mvaddch(y, x, main_char);
-      refresh();
-    } else if (ch == KEY_RIGHT) {
-      erasing(y, x);
-      if(map.getChar(x + 1, y) == '.') {
-        x = x + 1;
-      }
-      mvaddch(y, x, main_char);
-      refresh();
-    } else if (ch == KEY_UP) {
-      erasing(y, x);
-      if(map.getChar(x, y - 1) == '.') {
-        y = y - 1;
-      }
-      mvaddch(y, x, main_char);
-      refresh();
-    } else if (ch == KEY_DOWN) {
-      erasing(y, x);
-      if(map.getChar(x, y + 1) == '.') {
-        y = y + 1;
-      }
-      mvaddch(y, x, main_char);
-      refresh();
-    } else if (ch == 'q' || 'Q') {
-      break;
-    }
-  }
-}
-
-void erasing(int y, int x) { mvaddch(y, x, ' '); }
